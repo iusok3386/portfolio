9f475c6f41ac0ecf32a46b9d31a6ec2b007806e1
iusok3386 <example@example>
2024-04-22 18:54:38 +0900

    Practice

--- app/property.ts
+++ app/property.ts
@@ -1,4 +1,4 @@
-export const yourName = "You";
+export const yourName = "iusok3386";
 export const twitterLink = "https://twitter.com/"
 export const instagramLink = "https://instagram.com/"
-export const githubLink = "https://instagram.com/"
+export const githubLink = "https://github.com/iusok3386/"
